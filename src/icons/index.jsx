51f2090cef645ef0314ef3299b4035c2b4f3f7d6
nhptrangg <trangg1104@gmail.com>
2023-12-30 18:05:48 +0700

    Delete index.jsx

--- src/icons/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { FinalMyGrowth } from "./screens/FinalMyGrowth";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<FinalMyGrowth />);
